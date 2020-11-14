d8b785a641442684cedc1aa16b721378a29354c7
arpitmaurya01 <arpitmaurya01codechef@gmail.com>
2020-11-14 14:26:31 +0530

    Erro Suggestor added

---
+++ src/ErrorSuggest.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.*;
+public class ErrorSuggest {
+
+    /*
+
+        Suggest outputs from token db if the invalid token is similar to one in tokendb
+        Example:
+         intput > frdi : 60 ;
+         output > Syntax error : "frdi"
+                  Do you mean : "frd" ?
+        
+    */
+   static ArrayList<String> preserved_keywords = new ArrayList<>() {
+        {
+            add("legit");
+            add("arpit");
+            add("hello");
+            add("temper");
+            add("enter");
+
+        }
+    };
+
+   static void suggest(String source){
+       String[] tokens = source.split("");
+       // System.out.println(Arrays.toString(tokens));
+
+        int source_length = source.length();
+        for (int i = 0 ; i < preserved_keywords.size() ; ++i){
+            int match_count = 0;
+            int not_matched = 0;
+
+            String curr_keyword = preserved_keywords.get(i);
+           // System.out.println(curr_keyword);
+            
+            String ss[] = splitStrings(curr_keyword);
+            //System.out.println(Arrays.toString(ss));
+            for (int j = 0 ; j < tokens.length; j++){
+                
+                if (tokens[j].equals(ss[j])){
+                    match_count++;
+                }else{
+                   // System.out.println(tokens[j] +" "+ ss[j]);
+
+                    not_matched ++;
+                
+                }
+            }
+
+           // System.out.println(curr_keyword+" : Matched >"+match_count+" Not Matched :"+not_matched);
+           
+           if (match_count>not_matched){
+                System.out.println("<< Syntax error : "+source+" \n>> Do you meant : "+curr_keyword+"?");
+                
+           }
+
+        }
+
+       
+    }
+
+
+    static String[] splitStrings(String s){
+
+        String splitString[] = s.split("");
+
+        return splitString;
+    }
+
+
+
+    public static void main(String[] args) {
+        suggest("arrft");
+    }
+
+    
+}
